b0cff83050d097267bac3ba1198cc1d763fe4082
rafspiny <rafspiny@gmail.com>
2022-11-22 23:49:32 +0100

    Make shellcheck happy, for the time being

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,10 @@ profile="$INPUT_PROFILE"
 portage_version="$INPUT_PORTAGE_VERSION"
 gentoo_repo="$INPUT_GENTOO_REPO"
 
+echo "Path has been set to $path"
+echo "Profile has been set to $profile"
+echo "portage_version has been set to $portage_version"
+echo "gentoo_repo has been set to $gentoo_repo"
 echo "Running 'repoman full $repoman_args' from $(pwd)"
 # shellcheck disable=SC2086
 repoman full $repoman_args
